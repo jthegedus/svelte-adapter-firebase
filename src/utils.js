36210e9cea1620c0a1c239209566e92979584bfe
jthegedus <jthegedus@hey.com>
2021-03-22 17:58:38 +1100

    fix: test migration to cjs. fix xo node/no-path-concat rule

--- src/utils.js
+++ src/utils.js
@@ -23,7 +23,7 @@ function isString(parameter) {
  * 	publicDir: string
  * }}
  */
-export function parseFirebaseConfiguration({hostingSite, sourceRewriteMatch, firebaseJson}) {
+function parseFirebaseConfiguration({hostingSite, sourceRewriteMatch, firebaseJson}) {
 	if (!existsSync(firebaseJson)) {
 		throw new Error(`File ${firebaseJson} does not exist. The provided file should exist and be a Firebase JSON config.`);
 	}
@@ -117,7 +117,7 @@ export function parseFirebaseConfiguration({hostingSite, sourceRewriteMatch, fir
  * @param {string} serviceId
  * @returns {boolean} `true` if valid
  */
-export function validCloudRunServiceId(serviceId) {
+function validCloudRunServiceId(serviceId) {
 	return /^[a-z\d][a-z\d-]+[a-z\d]$/gm.test(serviceId) && serviceId.length < 64;
 }
 
@@ -133,7 +133,7 @@ export function validCloudRunServiceId(serviceId) {
  * - https://github.com/firebase/firebase-tools/blob/1633f4fccbbc1bcbc6216fe13b8e888c8940bde4/src/deploy/functions/validate.ts#L38
  * - https://github.com/firebase/firebase-tools/blob/2dc7216a498dee2ca7e2acc33d6ba16d5647e27f/src/extractTriggers.js#L18
  */
-export function validCloudFunctionName(name) {
+function validCloudFunctionName(name) {
 	return /^\w{1,62}$/.test(name);
 }
 
@@ -142,8 +142,15 @@ export function validCloudFunctionName(name) {
  * @param {string} filename source file path
  * @param {string} destDir destination directory
  */
-export function copyFileIfExistsSync(filename, destDir) {
+function copyFileIfExistsSync(filename, destDir) {
 	if (existsSync(filename)) {
 		copyFileSync(filename, path.join(destDir, filename));
 	}
 }
+
+module.exports = {
+	parseFirebaseConfiguration,
+	validCloudRunServiceId,
+	validCloudFunctionName,
+	copyFileIfExistsSync
+};
