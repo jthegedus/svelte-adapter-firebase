222edc08d369efe787dc2841110901bb05e95889
jthegedus <jthegedus@hey.com>
2021-05-16 13:25:10 +1000

    test: custom cloud run build example

---
+++ examples/run_custom_build_dir/svelte.config.js
@@ -0,0 +1,14 @@
+import firebase from 'svelte-adapter-firebase';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// Hydrate the <div id="svelte"> element in src/app.html
+		adapter: firebase({
+			cloudRunBuildDir: 'custom-cloud-run-build-dir'
+		}),
+		target: '#svelte'
+	}
+};
+
+export default config;
