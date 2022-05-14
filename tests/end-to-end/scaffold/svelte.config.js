2fd957bb052daec91d9a8295d942dba8e40169af
Juan Rodriguez <jrodriguez@isotalent.com>
2022-05-13 23:53:38 -0500

    update node to v16

--- tests/end-to-end/scaffold/svelte.config.js
+++ tests/end-to-end/scaffold/svelte.config.js
@@ -4,9 +4,7 @@ import firebase from 'svelte-adapter-firebase';
 const config = {
 	kit: {
 		// Hydrate the <div id="svelte"> element in src/app.html
-		adapter: firebase({
-			target: 'demo'
-		})
+		adapter: firebase()
 	}
 };
 
--- tests/integration/functions_single_site/svelte.config.js
+++ tests/integration/functions_single_site/svelte.config.js
@@ -3,7 +3,6 @@ import firebase from 'svelte-adapter-firebase';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// Hydrate the <div id="svelte"> element in src/app.html
 		adapter: firebase(),
 	}
 };
